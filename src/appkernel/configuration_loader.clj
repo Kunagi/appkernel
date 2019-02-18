aec2f54c142d0023edd7492deb3b43a994ef04fe
Witoslaw Koczewski <wi@koczewski.de>
2019-02-18 10:00:04 +0100

    data-dir

--- src/appkernel/configuration_loader.clj
+++ src/appkernel/configuration_loader.clj
@@ -6,19 +6,21 @@
 
 (defn load-configuration
   [app-name]
-  (let [app-name (paths/configs-dir app-name)
-        configs-dir app-name
-        file (io/as-file (str configs-dir "/config.edn"))
-        path (.getAbsolutePath file)]
-    (tap> [::configs-dir configs-dir])
+  (let [config-dir (paths/config-dir app-name)
+        data-dir (paths/data-dir app-name)
+        file (io/as-file (str config-dir "/config.edn"))
+        path (.getAbsolutePath file)
+        config {:app/config-dir config-dir
+                :app/data-dir data-dir}]
+    (tap> [::config-dir config-dir])
     (if (.exists file)
       (try
-        (let [config (read-string (slurp file))]
-          (tap> [::load-configuration {:file path :config config}])
-          config)
+        (let [config-edn (read-string (slurp file))]
+          (tap> [::load-configuration {:file path :config config-edn}])
+          (merge config-edn config))
         (catch Exception ex
           (throw (ex-info (str "Failed to read file " (.getAbsolutePath file))
                           {:file file}))))
       (do
         (tap> [::load-configuration {:file path :config nil}])
-        nil))))
+        config))))
--- src/appkernel/paths.clj
+++ src/appkernel/paths.clj
@@ -1,8 +1,13 @@
 (ns appkernel.paths)
 
-(defn configs-dir
+(defn config-dir
   [app-name]
   (let [config-file (java.io.File. "config.edn")]
     (if (.exists config-file)
       (-> config-file .getAbsoluteFile .getParentFile .getPath)
       (str "/etc/" app-name))))
+
+
+(defn data-dir
+  [app-name]
+  (-> "user.home" System/getProperty java.io.File. .getPath))
